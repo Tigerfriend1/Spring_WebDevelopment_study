8ec1aef1c0f0800eade4e21dfe8539572f5fa386
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-03-29 20:46:49 +0900

    upload: ex1

---
+++ ex1-spring/src/main/java/ex1/ex1spring/Ex1SpringApplication.java
@@ -0,0 +1,13 @@
+package ex1.ex1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex1SpringApplication.class, args);
+	}
+
+}
---
+++ ex1-spring/src/test/java/ex1/ex1spring/Ex1SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ex1.ex1spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
