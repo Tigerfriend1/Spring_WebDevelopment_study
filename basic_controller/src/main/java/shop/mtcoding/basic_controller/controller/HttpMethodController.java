0f312e1fa0b297029329fcaac0a9b8b61d7efd21
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-10-07 16:58:50 +0900

    feat : methodGetV4 : @ResponseBody사용

--- basic_controller/src/main/java/shop/mtcoding/basic_controller/controller/HttpMethodController.java
+++ basic_controller/src/main/java/shop/mtcoding/basic_controller/controller/HttpMethodController.java
@@ -2,6 +2,7 @@ package shop.mtcoding.basic_controller.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,7 +23,7 @@ public class HttpMethodController {
     public void methodGetV2(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("methodGetV2() 호출됨");
 
-        PrintWriter pw = response.getWriter();
+        PrintWriter pw = response.getWriter(); //BW버퍼에 직접 입력함.
         pw.println("<h1>Get</h1>"); //Content-Type은 text/html, Status Code는 200
     }
 
@@ -30,5 +31,10 @@ public class HttpMethodController {
     @GetMapping("/req/get/v3")
     public String methodGetV3() {
         return "hello"; //templates안에 hello.mustache를 찾으러 간다. // 그리고 ViewResolver가 발동됨.
+    }
+    @ResponseBody //나는 return시 ViewResolver말고 문자 그대로 표시하고 싶다!
+    @GetMapping("/req/get/v4")
+    public String methodGetV4() {
+        return "<h1>Get</h1>"; //return시 MessageConverter가 발동함.
     }
 }
