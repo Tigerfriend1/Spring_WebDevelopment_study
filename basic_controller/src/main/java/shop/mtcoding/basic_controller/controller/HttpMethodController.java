abab810158fd6ec1e0042ad5786afc93df12abf8
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-10-07 16:36:05 +0900

    feat : HttpMethodController

--- basic_controller/src/main/java/shop/mtcoding/basic_controller/controller/HttpMethodController.java
+++ basic_controller/src/main/java/shop/mtcoding/basic_controller/controller/HttpMethodController.java
@@ -1,5 +1,10 @@
 package shop.mtcoding.basic_controller.controller;
 
-public class HttpMethodController {
+import org.springframework.stereotype.Controller;
 
+@Controller //컴퍼넌트 스캔(디스패처 서블릿이 찾아준다.)
+public class HttpMethodController {
+    public HttpMethodController() {
+        System.out.println("컴퍼넌트 스캔됨");
+    }
 }
